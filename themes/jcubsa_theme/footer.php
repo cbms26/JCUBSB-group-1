a5b61684d075c318ac9fb62989c6cf9550fb7ba7
Ngawang Tenzin <ngawang.tenzin1@my.jcu.edu.au>
2024-08-09 19:19:18 +1000

    footer

--- themes/jcubsa_theme/footer.php
+++ themes/jcubsa_theme/footer.php
@@ -32,10 +32,50 @@
 
 <script>
   document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
+    // get a cookie by name
+    function getCookie(name) {
+      let cookieArr = document.cookie.split(";");
+      for (let i = 0; i < cookieArr.length; i++) {
+        let cookiePair = cookieArr[i].split("=");
+        if (name == cookiePair[0].trim()) {
+          return decodeURIComponent(cookiePair[1]);
+        }
+      }
+      return null;
+    }
+
+    // setting cookie to know user's first visti to the site
+    function setCookie(name, value, days) {
+      let date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      document.cookie = name + "=" + encodeURIComponent(value) + ";expires=" + date.toUTCString() + ";path=/";
+    }
+
+    // Checking if the loader cookie exists
+    if (!getCookie("loaderDisplayed")) {
+      // Display the loader
+      document.querySelector('.home-page-loader-wrapper').style.display = 'flex';
+      document.querySelector('.content-wrapper').style.display = 'none';
+
+      setTimeout(function() {
+        document.querySelector('.home-page-loader-wrapper').style.display = 'none';
+        document.querySelector('.content-wrapper').style.display = 'block';
+        // Setting loader cookie from displaying again
+        setCookie("loaderDisplayed", "true", 365);
+      }, 2000); // 2000 milliseconds = 2 seconds
+    } else {
+      // If the cookie exists, hide the loader and show the content immediately
       document.querySelector('.home-page-loader-wrapper').style.display = 'none';
       document.querySelector('.content-wrapper').style.display = 'block';
-    }, 500); // 2000 milliseconds = 2 seconds
+    }
+
+    // Smooth scrolling for "Scroll for more" link
+    document.querySelector('.scroll-down a').addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector('#below-content').scrollIntoView({ 
+        behavior: 'smooth' 
+      });
+    });
   });
 </script>
 
